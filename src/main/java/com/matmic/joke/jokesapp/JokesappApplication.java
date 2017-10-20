beaf53174394456e223d57f3fac1db93c9b88f92
MatMic <trajanesco@gmail.com>
2017-10-21 00:52:45 +0200

    Service and Controller added

--- src/main/java/com/matmic/joke/jokesapp/JokesappApplication.java
+++ src/main/java/com/matmic/joke/jokesapp/JokesappApplication.java
@@ -3,6 +3,7 @@ package com.matmic.joke.jokesapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class JokesappApplication {
 
